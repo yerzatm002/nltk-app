116af13ff45ad655970f72c0d2ce90e9d1514d5c
yerzatm002 <erzatnis@gmail.com>
2025-05-26 01:02:33 +0500

    adads

--- .render/build.sh
+++ .render/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 apt-get update && apt-get install -y poppler-utils tesseract-ocr tesseract-ocr-kaz
+pip install --upgrade pip
 pip install -r requirements.txt
-python -m nltk.downloader punkt
+python -c "import nltk; nltk.download('punkt')"
